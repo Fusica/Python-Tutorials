e2873d2ed6a52f9e4eef9ffc9f5d7f3fd900d96e
“Max” <1733169585a@gmail.com>
2021-06-16 13:48:01 +0800

    03 commit

---
+++ Strings.py
@@ -0,0 +1,11 @@
+# Editor: Max
+
+# Create Time: 6/15/21 11:03
+
+phrase = "Giraffe Academy"
+# print("\"Giraffe Academy\"")
+# print(phrase + " is cool")
+print(phrase.lower())  # convert it into lower case
+print(phrase.upper())  # convert it into upper case
+print(phrase.isupper())  # give a bool variable show if this sentence all upper case
+print(phrase.upper().isupper())
---
+++ Variables&Data-Types.py
@@ -0,0 +1,12 @@
+# Editor: Max
+
+# Create Time: 6/15/21 10:49
+
+character_name = "Tom"
+character_age = "40"
+print("There was a man name " + character_name + ",")
+print("he was " + character_age + " years old.")
+
+character_name = "Mike"
+print("He really liked the game,named " + character_name)
+print("but he didn't like being " + character_age + ".")
