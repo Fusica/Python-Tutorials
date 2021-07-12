d0aba5649727eeb0cf94881a5f7fea5620adfe64
max <1733169585a@gmail.com>
2021-07-12 11:36:56 +0800

    06 commit

--- Calculator.py
+++ Calculator.py
@@ -1,5 +1,10 @@
+# Editor: Max
+
+# Create Time: 7/11/21 15:00
+
 num1 = input("Enter a number: ")
 num2 = input("Enter another number: ")
 
-result = num1 + num2
+# result = int(num1) + int(num2)
+result = float(num1) + float(num2)
 print(result)
---
+++ Mad Libs.py
@@ -0,0 +1,8 @@
+# Editor: Max
+
+# Create Time: 7/11/21 17:12
+
+print("Roses are {color}")
+print("{plural noun} are blue")  # 复数名词
+print("I love {celebrity}")
+
