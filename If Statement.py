7c4611c2b48d2c8461e864e986ff872436dd350f
max <1733169585a@gmail.com>
2021-07-18 16:12:39 +0800

    09 commit

---
+++ If Statement.py
@@ -0,0 +1,10 @@
+# Editor: Max
+
+# Create Time: 7/18/21 3:28 PM
+
+is_male = False
+
+if is_male:
+    print("You are a male")
+else:
+    print("You are a female")
--- Return Statement.py
+++ Return Statement.py
@@ -2,4 +2,11 @@
 
 # Create Time: 7/18/21 11:25 AM
 
+def cube(num):
+    # result = pow(num, 3)
+    # return result
+    return pow(num, 3)
 
+
+a = input("Enter a num: ")
+print(cube(int(a)))
